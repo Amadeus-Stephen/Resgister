955e6b81944abf4f9fd2831a37235bafe23352ec
Amadeus Stephen <Amadeus-Stephen>
2020-12-13 06:17:41 -0500

    commit

--- backup/backend/src/main/java/API/jwt/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package API.jwt;
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
-
